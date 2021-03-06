6d8c61d2c3a615600d0d2277f41574eb18bb38d1
Fraser Carter <fraserc97@hotmail.co.uk>
2021-03-06 23:18:58 +0000

    set files up ready for webGL

--- sauce/render.ts
+++ sauce/render.ts
@@ -3,16 +3,7 @@ export class Renderer {
     canvas: any;
 
     constructor() {
-        this.canvas = document.createElement("CANVAS");
-        this.canvas.width = 200;
-        this.canvas.height = 200;
-
-        let context = this.canvas.getContext("2d");
-
-        context.fillStyle = "green";
-        context.fillRect(0,0, this.canvas.width, this.canvas.height);
-
-        document.body.appendChild(this.canvas);
+        this.canvas = document.createElement("canvas");
 
     }
 }
